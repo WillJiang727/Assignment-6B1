907b7df2f998e548f45f5eafae399f26f271e976
WillJiang727 <99672382+WillJiang727@users.noreply.github.com>
2022-03-28 10:27:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ document.getElementById("appointmentCount").innerHTML = currAppCount;
 //function to change color and register for calendar
 function bookButtonr1c1() {
     document.getElementById("r1c1").className = "booked";
+    confirm("Confirm your appointment: Tartan testing: Sunday, March 6, 2022 8:00 AM @TCS Hall (Tata Consultancy Services)")
     if (localStorage.getItem("currAppCount") != null)
     {
         currAppCount += 1;
@@ -30,6 +31,7 @@ function bookButtonr1c1() {
 
 function bookButtonr2c2() {
     document.getElementById("r2c2").className = "booked";
+    confirm("Confirm your appointment: Tartan testing: Monday, March 7, 2022 8:05 AM @TCS Hall (Tata Consultancy Services)")
     currAppCount += 1;
     document.getElementById("appointmentCount").innerHTML = currAppCount;
 }
@@ -37,6 +39,7 @@ function bookButtonr2c2() {
 
 function bookButtonr2c7() {
     document.getElementById("r2c7").className = "booked";
+    confirm("Confirm your appointment: Tartan testing: Saturdat, March 12, 2022 8:05 AM @TCS Hall (Tata Consultancy Services)")
     currAppCount += 1;
     document.getElementById("appointmentCount").innerHTML = currAppCount;
 }
@@ -44,6 +47,7 @@ function bookButtonr2c7() {
 
 function bookButtonr4c1() {
     document.getElementById("r4c1").className = "booked";
+    confirm("Confirm your appointment: Tartan testing: Sunday, March 6, 2022 8:15 AM @TCS Hall (Tata Consultancy Services)")
     currAppCount += 1;
     document.getElementById("appointmentCount").innerHTML = currAppCount;
 }
