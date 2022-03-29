03882a1c96397ccca966ce0a67675e725c61d624
WillJiang727 <99672382+WillJiang727@users.noreply.github.com>
2022-03-29 13:21:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 /*** Object Constructors ***/
-//function appointment(type, time, location) {
-//  this.type = type;
-//  this.time = time;
-//  this.location = location;
-//}
+function Appointment(time, date, location,type) {
+  this.type = type;
+  this.date = date;
+  this.time = time;
+  this.location = location;
+}
+var appts = []
+var appt_1 = new Appointment("8:00","March 6");
+var appt_2= new Appointment("8:00","March 7");
+var appt_3 = new Appointment("8:00","March 6");
+// till March 12
+var appts = [appt_1,appt_2,appt_3];
+// update appts
 
 //assign global variable for counting appointments
 let currAppCount = 0;
 document.getElementById("appointmentCount").innerHTML = currAppCount;
 
 
+function createList(){
+    var first_row = document.getElementById("first-row");
 
+
+    let newItem = document.createElement("li");
+    newItem.innerHTML = '8:00 - 8:05';
+    first_row.appendChild(newItem);
+    // for loop over appts
+    let newItem2 = document.createElement("li");
+    newItem2.innerHTML = '<button class="booked">TCS Hall</button>';
+    first_row.appendChild(newItem2);
+
+}
+
+function displayCount(currAppCount) {
+    // body...
+
+    document.getElementById("appointmentCount").innerHTML = currAppCount;
+}
 
 //function to change color and register for calendar
 function bookButtonr1c1() {
@@ -18,8 +44,10 @@ function bookButtonr1c1() {
     confirm("Confirm your appointment: Tartan testing: Sunday, March 6, 2022 8:00 AM @TCS Hall (Tata Consultancy Services)")
     if (localStorage.getItem("currAppCount") != null)
     {
-        currAppCount += 1;
-        document.getElementById("appointmentCount").innerHTML = currAppCount;
+        local_storage_value = parseInt(localStorage.getItem("currAppCount"));
+        currAppCount += 1+local_storage_value;
+        displayCount(currAppCount);
+        localStorage.setItem("currAppCount", currAppCount);
     }
     else
     {
@@ -50,6 +78,12 @@ function bookButtonr4c1() {
     confirm("Confirm your appointment: Tartan testing: Sunday, March 6, 2022 8:15 AM @TCS Hall (Tata Consultancy Services)")
     currAppCount += 1;
     document.getElementById("appointmentCount").innerHTML = currAppCount;
+}
+
+window.onload = function(){
+    let local_storage_value = parseInt(localStorage.getItem("currAppCount"));
+    displayCount(local_storage_value);
+
 }
 
 
